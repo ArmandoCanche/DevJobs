674a668605190078dbf067ec031885c7c8908a98
Armando Tintore <jorgecanche2707@gmail.com>
2025-10-14 22:29:22 -0600

    Filtros de la ventana de Emepleos

--- semana01/js/script.js
+++ semana01/js/script.js
@@ -15,10 +15,49 @@ const jobsListingSection = document.querySelector('.jobs-listings')
 jobsListingSection.addEventListener('click', (event) =>{
     const element = event.target
 
-
     if(element.classList.contains('button-apply-job')){
         element.textContent = '¡Aplicado!',
         element.classList.add('is-applied')
         element.disabled = true
     }
+
+    const firstJob = document.querySelector('.job-listing-card')
+
+    if(firstJob.contains(element)){
+        window.location.href ='../html/resultados.html'
+    }
 })
+
+
+const selectTecnologias = document.getElementById('filtro-tecnologias');
+const selectUbicacion = document.getElementById('filtro-ubicacion');
+const selectContrato = document.getElementById('filtro-contrato');
+const selectExperiencia = document.getElementById('filtro-experiencia');
+const articulos = document.querySelectorAll('.jobs-listings article');
+
+function filtrarTrabajos(){
+    const tecnologia = selectTecnologias.value;
+    const ubicacion = selectUbicacion.value;
+    const contrato = selectContrato.value;
+    const experiencia = selectExperiencia.value;
+
+    articulos.forEach((articulo) => {
+        const cumpleTecnologia = tecnologia === "Tecnologías" || articulo.dataset.tecnologia === tecnologia;
+        const cumpleUbicacion = ubicacion === "Ubicacion" || articulo.dataset.ubicacion === ubicacion;
+        const cumpleContrato = contrato === "Contrato" || articulo.dataset.contrato === contrato;
+        const cumpleExperiencia = experiencia === "NivelExperiencia" || articulo.dataset.experiencia === experiencia;
+
+
+        if(cumpleTecnologia && cumpleUbicacion && cumpleContrato && cumpleExperiencia){
+            articulo.style.display = 'flex';
+        } else{
+            articulo.style.display = 'none';
+        }
+    });
+}
+
+
+selectTecnologias.addEventListener('change',filtrarTrabajos);
+selectUbicacion.addEventListener('change',filtrarTrabajos);
+selectContrato.addEventListener('change',filtrarTrabajos);
+selectExperiencia.addEventListener('change',filtrarTrabajos);
